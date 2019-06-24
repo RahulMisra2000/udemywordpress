977e455cea51ce80d72428c52b56089fbd5bb008
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-06-24 13:40:33 -0400

    ACF 's custom field contains post objects

--- fictional-university-theme/single-event.php
+++ fictional-university-theme/single-event.php
@@ -15,8 +15,12 @@
       <div class="generic-content"><?php the_content(); ?></div>
 
       <?php
-
+        
+        // ---------------------------------------------------------------------------------------------------
         // related_programs is the custom field created in the Events custom post type by using the ACF plugin
+        // In the dashboard after creating it, we went into the event record and in the custom field we selected 
+        // one or more records from the Program post type. 
+        // The related_programs will contain **post objects** for all the programs that were selected
         // ---------------------------------------------------------------------------------------------------
         $relatedPrograms = get_field('related_programs');
 
@@ -24,9 +28,13 @@
           echo '<hr class="section-break">';
           echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
           echo '<ul class="link-list min-list">';
+          
+          // ********* In this loop we will get all the Program records as **POST OBJECTS** that were assigned to the event
+          // ********* Because they are post objects, we can use them in functions like get_the_permalink() etc
           foreach($relatedPrograms as $program) { ?>
-            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+                <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
           <?php }
+          
           echo '</ul>';
         }
 
