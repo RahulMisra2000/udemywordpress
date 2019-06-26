97bd28789cab45d34da217c622fd8e1ee2ede31b
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-06-26 14:15:09 -0400

    API gathers results & places them in nested arrays
    When WP receives the custom API url, it will loop thru the relevant posts, custom post types and pages and places the results in different arrays that will be returned to the caller of the API. WP will automatically convert the php data structure array into a json array and php associative array into a json object.

--- fictional-university-theme/inc/search-route.php
+++ fictional-university-theme/inc/search-route.php
@@ -28,8 +28,9 @@ function universitySearchResults($data) {
   // ************* Creating an associative array in WP ... think of it as analogous to javascript object *****************
   // => is just like : in javascript's literal object definition.       
   // DETOUR:    in php -> is the same as javascript's dot notation to go inside an object
-  $results = array(
-    'generalInfo' => array(),
+  $results = array(                       // *** creating a nested array
+    'generalInfo' => array(),                 // blog posts and pages info will be stuffed in here
+                                              // Info pertaining to the custom Post types will be in the next 4 arrays
     'professors' => array(),
     'programs' => array(),
     'events' => array(),
@@ -37,80 +38,83 @@ function universitySearchResults($data) {
   );
 
   while($mainQuery->have_posts()) {
-    $mainQuery->the_post();
+    $mainQuery->the_post();                           // ***** this allows the template tags like get_the_title() to access 
+                                                      //       data in the returned record   ++++++++
 
     if (get_post_type() == 'post' OR get_post_type() == 'page') {
-      array_push($results['generalInfo'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink(),
-        'postType' => get_post_type(),
-        'authorName' => get_the_author()
-      ));
+          array_push($results['generalInfo'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'postType' => get_post_type(),
+            'authorName' => get_the_author()
+          ));
     }
 
     if (get_post_type() == 'professor') {
-      array_push($results['professors'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink(),
-        'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
-      ));
+          array_push($results['professors'], array(
+            'title' => get_the_title(),                   // ****** this says get the title from the 
+            'permalink' => get_the_permalink(),
+            'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+          ));
     }
 
     if (get_post_type() == 'program') {
-      $relatedCampuses = get_field('related_campus');
+          $relatedCampuses = get_field('related_campus'); // *** related_campus - this is the ACF field that points to the Campus custom post type
+          // ********* So, $relatedCampuses is an array of ALL the Campus records associated with the current record in the loop
 
-      if ($relatedCampuses) {
-        foreach($relatedCampuses as $campus) {
-          array_push($results['campuses'], array(
-            'title' => get_the_title($campus),
-            'permalink' => get_the_permalink($campus)
+          if ($relatedCampuses) {
+            foreach($relatedCampuses as $campus) {          // $campus is the individual campus record
+              array_push($results['campuses'], array(
+                'title' => get_the_title($campus),          // ****** this says get the title from the campus record and NOT from +++++++++
+                                                            //        which it would if we didn't specify the $campus
+                'permalink' => get_the_permalink($campus)
+              ));
+            }
+          }
+
+          array_push($results['programs'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'id' => get_the_id()
           ));
-        }
-      }
-    
-      array_push($results['programs'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink(),
-        'id' => get_the_id()
-      ));
     }
 
     if (get_post_type() == 'campus') {
-      array_push($results['campuses'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink()
-      ));
-    }
+          array_push($results['campuses'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+          ));
+        }
 
     if (get_post_type() == 'event') {
-      $eventDate = new DateTime(get_field('event_date'));
-      $description = null;
-      if (has_excerpt()) {
-        $description = get_the_excerpt();
-      } else {
-        $description = wp_trim_words(get_the_content(), 18);
-      }
+          $eventDate = new DateTime(get_field('event_date'));   // *** Wrapping it in DateTime so we can use helper methods
+          $description = null;
+          if (has_excerpt()) {
+            $description = get_the_excerpt();
+          } else {
+            $description = wp_trim_words(get_the_content(), 18);
+          }
 
-      array_push($results['events'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink(),
-        'month' => $eventDate->format('M'),
-        'day' => $eventDate->format('d'),
-        'description' => $description
-      ));
+          array_push($results['events'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'month' => $eventDate->format('M'),               // *** this is why we wrapped it in DateTime
+            'day' => $eventDate->format('d'),
+            'description' => $description
+          ));
     }
     
   }
 
   if ($results['programs']) {
-    $programsMetaQuery = array('relation' => 'OR');
+        $programsMetaQuery = array('relation' => 'OR');
 
-    foreach($results['programs'] as $item) {
-      array_push($programsMetaQuery, array(
-          'key' => 'related_programs',
-          'compare' => 'LIKE',
-          'value' => '"' . $item['id'] . '"'
-        ));
+        foreach($results['programs'] as $item) {
+              array_push($programsMetaQuery, array(
+                  'key' => 'related_programs',
+                  'compare' => 'LIKE',
+                  'value' => '"' . $item['id'] . '"'
+                ));
     }
 
     $programRelationshipQuery = new WP_Query(array(
