5b3ad8ed43945b64efb5dede4bc93fae43443590
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-07-05 13:17:11 -0400

    Comments

--- fictional-university-theme/page-my-notes.php
+++ fictional-university-theme/page-my-notes.php
@@ -16,38 +16,49 @@
 
     <div class="container container--narrow page-section">
       
-      <div class="create-note">
-        <h2 class="headline headline--medium">Create New Note</h2>
-        <input class="new-note-title" placeholder="Title">
-        <textarea class="new-note-body" placeholder="Your note here..."></textarea>
-        <span class="submit-note">Create Note</span>
-        <span class="note-limit-message">Note limit reached: delete an existing note to make room for a new one.</span>
-      </div>
+        <div class="create-note">
+          <h2 class="headline headline--medium">Create New Note</h2>
+          <input class="new-note-title" placeholder="Title">
+          <textarea class="new-note-body" placeholder="Your note here..."></textarea>
+          <span class="submit-note">Create Note</span>
+          <span class="note-limit-message">Note limit reached: delete an existing note to make room for a new one.</span>
+        </div>
 
-      <ul class="min-list link-list" id="my-notes">
-        <?php 
-          $userNotes = new WP_Query(array(
-            'post_type' => 'note',
-            'posts_per_page' => -1,
-            'author' => get_current_user_id()
-          ));
+        <ul class="min-list link-list" id="my-notes">
+          <?php 
 
-          while($userNotes->have_posts()) {
-            $userNotes->the_post(); ?>
-            <li data-id="<?php the_ID(); ?>">
-              <input readonly class="note-title-field" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>">
-              <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
-              <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
-              <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
-              <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
-            </li>
-          <?php }
+            $userNotes = new WP_Query(array(
+                'post_type' => 'note',
+                'posts_per_page' => -1,
+                'author' => get_current_user_id()     // ******** THIS IS IMP .... where clause to make sure logged in user's notes *** */
+            ));
 
-        ?>
-      </ul>
+            while($userNotes->have_posts()) {
+                  $userNotes->the_post(); ?>
+
+                  <li data-id="<?php the_ID(); ?>">
+                    
+                      <!-- ************************ Title of Note********************************** ---->  
+                      <input readonly class="note-title-field" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>">
+                      <!-- ************************ EDIT Button *********************************** ---->  
+                      <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>  
+                    
+                      <!-- ************************ DELETE Button ********************************* ---->  
+                      <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>  <!-- DELETE ---->
+                    
+                      <!-- ************************ Body of Note ********************************** ---->  
+                      <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
+                    
+                      <!-- ************************ SAVE Button *********************************** ---->  
+                      <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
+                  </li>
+            <?php }
+
+          ?>
+        </ul>
     </div>
     
-  <?php }
+  <?php }     // ****** OUTER WHILE ******************/
 
   get_footer();
 
