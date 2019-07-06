f5662b24d85467d2dcc3333a392a3ad6f36e8870
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-07-06 00:43:40 -0400

    Comments

--- fictional-university-theme/single-professor.php
+++ fictional-university-theme/single-professor.php
@@ -40,24 +40,25 @@
     
     
     
-              $existStatus = 'no';
-              // ****** this query will return a record if the currently logged in user has already liked the prof showing on page
+              
+              // ****** This query will return a record if the logged in user has already liked the prof showing on page
+              $existStatus = 'no';          
               if (is_user_logged_in()) {
-                $existQuery = new WP_Query(array(
-                  'author' => get_current_user_id(),      // ****** id of the logged in user    
-                  'post_type' => 'like',
-                  'meta_query' => array(
-                    array(
-                      'key' => 'liked_professor_id',
-                      'compare' => '=',
-                      'value' => get_the_ID()
-                    )
-                  )
-                ));
+                      $existQuery = new WP_Query(array(
+                            'author' => get_current_user_id(),      // ****** id of the logged in user    
+                            'post_type' => 'like',
+                            'meta_query' => array(
+                              array(
+                                'key' => 'liked_professor_id',
+                                'compare' => '=',
+                                'value' => get_the_ID()
+                              )
+                            )
+                      ));
 
-                if ($existQuery->found_posts) {          // true if the current logged in user has already liked the prof
-                  $existStatus = 'yes';
-                }
+                    if ($existQuery->found_posts) {          // true if the current logged in user has already liked the prof
+                          $existStatus = 'yes';
+                    }
               }
 
               
@@ -81,13 +82,13 @@
         $relatedPrograms = get_field('related_programs');
 
         if ($relatedPrograms) {
-          echo '<hr class="section-break">';
-          echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
-          echo '<ul class="link-list min-list">';
-          foreach($relatedPrograms as $program) { ?>
-            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
-          <?php }
-          echo '</ul>';
+              echo '<hr class="section-break">';
+              echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
+              echo '<ul class="link-list min-list">';
+              foreach($relatedPrograms as $program) { ?>
+                <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+              <?php }
+              echo '</ul>';
         }
 
       ?>
